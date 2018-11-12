8cbef5481b06859f1039a0f8fd119547971ecc06
Vitor Henckel <vitor@henckel.com.br>
2018-11-12 11:15:36 -0200

    Fnalizando os testes de ReactJS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Routes from './routes';
+
 import './styles.css';
 
 import Header from './components/Header';
@@ -7,7 +9,7 @@ import Main from './pages/main';
 const App = () => (
    <div className="App">
       <Header />
-      <Main />
+      <Routes />
     </div>
 );
 
